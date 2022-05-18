7800a9448166f58293ff0eba7330ca9d57d4df70
VladSher1234 <volsherbnik@gmail.com>
2022-05-18 20:04:26 +0300

    Edits

---
+++ BuilderOfTheMagicSquare.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int size=0;
+     cout<<"Enter size square: ";
+     cin>>size;
+     cout<<'\n';
+ 
+     if (!(size & 1)) {
+         cerr<<"Size should be even";
+         cin.get();
+         exit(1);
+     }
+ 
+     int max_size=size*2-1;
+ 
+     int**pMagic_square = new  int*[max_size];
+     for (int i=0; i<max_size; i++)
+        pMagic_square[i]=new int[max_size];
+ 
+     int i;
+     int j;
+     for (i=0; i<max_size; i++)
+     for (j=0; j<max_size; j++)
+     pMagic_square[i][j]=0;
+      int k=1;
+     i=(max_size>>1);
+     j=0;
+     while (i<max_size){
+         int i1=i;
+         int j1=j;
+         int gr=size>>1;
+         while (j1<((j+(max_size>>1))+1)){
+             pMagic_square[i1][j1]=k;
+             if ((i1>(gr)) && (j1<(gr)))
+                pMagic_square[i1][j1+size]=k;
+             if ((i1>(size+gr)-1) && (j1>(gr)))
+                pMagic_square[i1-size][j1]=k;
+             if ((i1<(gr)) && (j1>(gr)))
+                pMagic_square[i1+size][j1]=k;
+             if ((i1>(gr)) && (j1>(size+gr)-1))
+                pMagic_square[i1][j1-size]=k;
+             --i1;
+             ++j1;
+             ++k;
+         }
+         ++i;
+         ++j;
+     }
+
+      cout << "Magic constant: " << (size * (size * size + 1)) / 2 << '\n';
+
+     cout<<endl<<"Magic square: "<<endl<<endl;
+     for (i=(size-1)>>1; i<=max_size-(size>>1)-1; i++ ){
+         for (j=(size-1)>>1; j<=max_size-(size>>1)-1; j++ )
+             cout << pMagic_square[i][j] << " ";
+         cout << endl;
+     }
+ 
+     for (int i = 0; i < max_size; ++i)
+        delete[] pMagic_square[i];
+        delete[] pMagic_square;
+ 
+     cin.get();
+}
---
+++ IsPalindrome.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReverseNumber(int n)
+{
+    int reversed_number = 0, remainder;
+    while(n != 0) 
+    {
+        remainder = n % 10;
+        reversed_number = reversed_number * 10 + remainder;
+        n /= 10;
+    }
+    return reversed_number;
+}
+
+bool IsPalindrome(int n)
+{
+    return (ReverseNumber(n) == n);
+}
+
+int main()
+{
+    int n;
+    cout << "Write the number: ";
+    cin >> n;
+    if (IsPalindrome(n))
+    {
+        cout << "Number is palindrome" << '\n';
+    }
+    else
+    {
+        cout << "Number is not palindrome" << '\n';
+    }
+}
