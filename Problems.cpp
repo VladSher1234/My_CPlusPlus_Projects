bca7c55a614304c637c9b7a4f133ace654f302e6
VladSher1234 <volsherbnik@gmail.com>
2022-05-25 16:47:28 +0300

    Edits

---
+++ Problems.cpp
@@ -0,0 +1,17 @@
+// This is problems from russian 
+// book by Boris Kordemsky 
+// "Удивительный мир чисел и фигур"
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // 1.9.2
+    for(int i = 0; i < 2000; i += 77)
+        if(i % 74 == 48)
+            cout << "1.9.2 solution: " << i << endl;
+    /// 
+    
+}
